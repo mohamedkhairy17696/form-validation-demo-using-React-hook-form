70dafd703d1b63268b0245bba7099c26eb92bf94
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-29 17:23:28 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -104,9 +104,9 @@ function App() {
               name="phoneNumber"
               id="phoneNumber"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-              placeholder="Phone number (123-456-7890)"
+              placeholder="Phone number (01060561050)"
               {...register("phoneNumber", {
-                pattern: /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/,
+                pattern: /^01[0125][0-9]{8}$/,
               })}
             />
             {errors.phoneNumber && (
