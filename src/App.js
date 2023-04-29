e6dbfd8f8e670eed9f25b7135065ffba81f9ff1b
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2023-04-29 17:17:11 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -57,8 +57,8 @@ function App() {
           <div class="relative z-0 w-full mb-6 group">
             <input
               type="text"
-              name="floating_first_name"
-              id="floating_first_name"
+              name="firstName"
+              id="firstName"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder="First name"
               {...register("firstName", {
@@ -78,8 +78,8 @@ function App() {
           <div class="relative z-0 w-full mb-6 group">
             <input
               type="text"
-              name="floating_first_name"
-              id="floating_first_name"
+              name="lastName"
+              id="lastName"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder="Last name"
               {...register("lastName", {
@@ -101,8 +101,8 @@ function App() {
           <div class="relative z-0 w-full mb-6 group">
             <input
               type="tel"
-              name="floating_phone"
-              id="floating_phone"
+              name="phoneNumber"
+              id="phoneNumber"
               class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
               placeholder="Phone number (123-456-7890)"
               {...register("phoneNumber", {
